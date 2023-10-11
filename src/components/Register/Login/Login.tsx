1edeb569ef45d32ab62ff1186f9c4bb7d916d71f
mustaphachqoubi <mustaphachqoubi88@gmail.com>
2023-10-11 22:34:58 +0100

    added the input field

--- src/components/Register/Login/Login.tsx
+++ src/components/Register/Login/Login.tsx
@@ -1,13 +1,30 @@
 import { styled } from 'styled-components'
 
-const LoginContainer = styled.div`
+const LoginContainer = styled.form`
  padding: 2rem 0rem;
- font-weight: bold;
- font-size: 2rem
+ display: flex;
+ justify-content: center;
+ align-items: center;
+ width: 100%;
+ flex-direction: column;
+ gap: 1rem;
+`
+
+const Input = styled.input`
+  border: 2px solid white;
+  border-radius: .3rem;
+  background: transparent;
+  padding: .5rem 2rem;
+  max-width: 20rem;
 `
 
 const Login = () => {
-  return <LoginContainer>Login</LoginContainer>;
+  return (
+  <LoginContainer>
+    <Input type="email" placeholder="Email"></Input>
+    <Input type="password" placeholder="Password"></Input>
+  </LoginContainer>
+  );
 };
 
 export default Login;
